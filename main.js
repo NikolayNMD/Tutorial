fc5673ea785b5c3b41b5a56061e675debecd4883
NikolayNMD <136238601+NikolayNMD@users.noreply.github.com>
2023-09-07 21:19:55 +0300

    2cnd commit

--- main.js
+++ main.js
@@ -1,21 +1,56 @@
+const products = [
+	{
+		'id': 1,
+		'name': 'iPhone 13 Pro',
+		'image': "https://www.reliancedigital.in/medias/Apple-MLL93HN-A-Smart-Phones-491997748-i-1-1200Wx1200H?context=bWFzdGVyfGltYWdlc3wyNTMwNDV8aW1hZ2UvanBlZ3xpbWFnZXMvaGM0L2g5YS85Nzc2MDg4NTgwMTI2LmpwZ3wxYjQ0OGE4NzlhN2MzNDljZDNmYjRkZjliZjlhMDEwYTBiM2JhOTZjNmM2YzQ0ZWY0NzU1OGMzZDA1OTY3YmE2",
+		'price': '27999'
+	},
+	{
+		'id': 2,
+		'name': 'MacBook Pro 14',
+		'image': "https://justbuy.com.ua/wp-content/uploads/2023/05/1268945_zoom-removebg-preview.png",
+		'price': '73999'
+	},
+	{
+		'id': 3,
+		'name': 'AirPods Pro',
+		'image': "https://hotline.ua/img/tx/211/2113157695.jpg",
+		'price': '8499'
+	},
+	{
+		'id': 4,
+		'name': 'Apple iPad Pro 12.9',
+		'image': "https://hotline.ua/img/tx/182/1821114155.jpg",
+		'price': '18999'
+	}
+]
 $(document).ready(function(){
-    $.ajax({
-        type: 'GET',
-        dataType:"json",
-        url: 'https://jsonplaceholder.typicode.com/posts',
-        success: function (data) {
-          for (let item of data) {
-            console.log(item);
-             $('.post_box').append(
-            '<div class="post_block">' + 
-                '<a href="post.html?id=' + item.id + '" class="post_block_title">' + 'title:' + item.title + '</a>' +
-                '<div class="post_user_id">' + 'userId:' + item.userId + '</div>' +
-                '<div class"post_id">' + 'postId:' + item.id + '</div>' +
-                '<div class="post_body">' + 'body:' + item.body + '</div>' +
-            '</div>');   
+	for (let item of products) {
+		let block = 
+		'<div class="product">' + 
+			'<div class="product_image">' + 
+				'<img src="' + item.image +  '" alt="phone">' +
+			'</div>' +
+			'<a href="#" class="product_title">' + item.name + '</a>' +
+			'<div class="product_code">Product Code: <span>' + item.id + '</span></div>' +
+			'<div class="product_nav">' +
+				'<div class="product_price">UAH <span>' + item.price + '</span></div>' +
+				'<button>Add to cart</button>' +
+			'</div>' +
+		'</div>';
 
-          }
-        }
-      });
+		$('.products').append(block);
+	}
 });
 
+{/* <div class="product">
+		<div class="product_image">
+			<img src="https://hotline.ua/img/tx/211/2113157695.jpg" alt="phone">
+		</div>
+		<a href="#" class="product_title">Air Pods Pro Max</a>
+		<div class="product_code">Product Code: <span>1</span></div>
+		<div class="product_nav">
+			<div class="product_price">UAH <span>10 000</span></div>
+			<button>Add to cart</button>
+		</div>
+</div> */}

77948583cf9f8c990c6fa4f9c087b6e859302c04
NikolayNMD <136238601+NikolayNMD@users.noreply.github.com>
2023-09-08 21:59:42 +0300

    4th commit

---
+++ cart.js
@@ -0,0 +1,16 @@
+const cart = {};
+cart['products'] = [
+    {
+		'id': 1,
+		'name': 'iPhone 13 Pro',
+		'image': "https://www.reliancedigital.in/medias/Apple-MLL93HN-A-Smart-Phones-491997748-i-1-1200Wx1200H?context=bWFzdGVyfGltYWdlc3wyNTMwNDV8aW1hZ2UvanBlZ3xpbWFnZXMvaGM0L2g5YS85Nzc2MDg4NTgwMTI2LmpwZ3wxYjQ0OGE4NzlhN2MzNDljZDNmYjRkZjliZjlhMDEwYTBiM2JhOTZjNmM2YzQ0ZWY0NzU1OGMzZDA1OTY3YmE2",
+		'price': '27999'
+	},
+	{
+		'id': 2,
+		'name': 'MacBook Pro 14',
+		'image': "https://justbuy.com.ua/wp-content/uploads/2023/05/1268945_zoom-removebg-preview.png",
+		'price': '73999'
+	}
+];
+cart['totalPrice'] = 101998;
